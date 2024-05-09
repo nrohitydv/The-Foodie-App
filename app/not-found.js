a619f5d739d8ad6a6247ef898c5be2cfa4af0d5b
Rohit <rohityadav787898@gmail.com>
2024-05-09 15:05:13 +0545

    Added an error and not-found page

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>Unfortunately, we could not found requested page or resource</p>
+    </main>
+  );
+}
+
+export default NotFound;
