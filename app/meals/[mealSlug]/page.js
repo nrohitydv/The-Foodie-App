60963e649f3869bfef174db808d562b9877c063a
Rohit <rohityadav787898@gmail.com>
2024-05-09 15:59:29 +0545

    meal detail page done

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,32 @@
-import React from "react";
-
-function MealDetails() {
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+function MealDetails({ params }) {
+  const meal = getMeal(params.mealSlug);
+  // for line breaks
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <div>
-      <h1>Meal Details</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt="" fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          //  dangerouslySetInnerHTML, as it can expose your application to cross-site scripting (XSS) attacks if the content is not properly sanitized.
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
 
